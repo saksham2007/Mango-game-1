3c90b4212d94803892ce2e336b715e996bcd6084
saksham2007 <70582707+saksham2007@users.noreply.github.com>
2020-12-28 18:28:32 +0530

    Update mango.js

--- mango.js
+++ mango.js
@@ -11,11 +11,12 @@ class Mango
         this.y=y
         this.r=r
 this.image=loadImage("images/mango.png")
-this.body=Bodies.circle(this.x,this.y,this.r,options)
+this.body=Bodies.circle(this.x,this.y,this.r/4,options)
 World.add(world,this.body)
     }
     display(){
+        var pos= this.body.position
         imageMode(CENTER)
-        image(this.image,this.x,this.y,this.r,this.r)
+        image(this.image,pos.x,pos.y,this.r,this.r)
     }
 }
